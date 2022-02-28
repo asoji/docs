d0b835638fa319c80d261640bf6d54bbdc5cfd7a
Devin <asoji-git@hiitsdevin.mozmail.com>
2022-02-28 01:53:48 -0800

    Some cleaning + Working on definitions.md

--- docs/controlpatcher/definitions.md
+++ docs/controlpatcher/definitions.md
@@ -1,23 +1,28 @@
 ---
-title: Definitions.cs Docs
+title: Definitions.cs
 ---
 
 # Definitions
+This file contains variables and other "definitions" that can be reused throughout the Patcher.
+
+## Legend
+- Search - What the Patcher looks for in the Vanilla game file.
+- Patch - What the Patcher patches the game with.
 
 ## Strings
 
 - `patcherVersion` - Patcher Version
 
-  - Read only value, please do not update unless you know what you're doing.
+    - Read only value, please do not update unless you know what you're doing.
 
 - `controlDir` - User selected directory for Control.exe
 
-  - Must be a valid directory with `Control.exe`, `Control_DX11.exe` and `Control_DX12.exe`
+    - Must be a valid directory with `Control.exe`, `Control_DX11.exe` and `Control_DX12.exe`
 
 - `dxVer` - User selected DirectX version
 
-  - DX11
-  - DX12
+    - DX11
+    - DX12
 
 - `gameVer` - User selected Control Version
 
@@ -25,84 +30,85 @@ title: Definitions.cs Docs
 
 - `IntroSkipPatch`
 
-  - Skips into in `ep100-000-pc.rmdp` file located in `controlDir/data_packfiles`
+    - Skips into in `ep100-000-pc.rmdp` file located in `controlDir/data_packfiles`
 
-    - Searches
+        - Search at offset(h) `224352020` - `224352030`
 
-      - ```csharp
-        ...0x53, 0x00, 0x58, 0x02, 0x00, 0x00, 0x10, 0xFC, 0x00, 0x00, 0x58, 0x02, 0x00...
-        ```
+            - ```csharp
+              ...0x53, 0x00, 0x58, 0x02, 0x00, 0x00, 0x10, 0xFC, 0x00, 0x00, 0x58, 0x02, 0x00...
+              ```
 
-    - Patches
+        - Patch
 
-      - ```csharp
-        ...0x53, 0x00, 0x03, 0x00, 0x00, 0x00, 0x10, 0xFC, 0x00, 0x00, 0x03, 0x00, 0x00...
-        ```
+            - ```csharp
+              ...0x53, 0x00, 0x03, 0x00, 0x00, 0x00, 0x10, 0xFC, 0x00, 0x00, 0x03, 0x00, 0x00...
+              ```
 
 - `SSE42CheckDisablePatch`
 
-  - Disables the SSE4.2 Check on CPUs that don't support SSE4.2 in `rl_rmdwin7_f.dll` [DX11] and `rl_rmdwin10_f.dll` [DX12]
+    - Disables the SSE4.2 Check on CPUs that don't support SSE4.2 in `rl_rmdwin7_f.dll` [DX11]
+      and `rl_rmdwin10_f.dll` [DX12]
 
-  - upgrade your processor please? why are you playing on an older processor that's not supported by Control?
+    - upgrade your processor please? why are you playing on an older processor that's not supported by Control?
 
-    - Searches
+        - Search at offset(h) `0005b8B0`
 
-      - ```csharp
-        ...0x14, 0x73, 0x0A, 0x8B...
-        ```
+            - ```csharp
+              ...0x14, 0x73, 0x0A, 0x8B...
+              ```
 
-    - Patches
+        - Patch
 
-      - ```csharp
-        ...0x14, 0x90, 0x90, 0x8B...
-        ```
+            - ```csharp
+              ...0x14, 0x90, 0x90, 0x8B...
+              ```
 
 - `PS4DLCPatchEpic` / `PS4DLCPatchSteamGOG`
 
-  - Gives PS4 Exclusive DLC in `Control_DX11.exe` and `Control_DX12.exe`
+    - Gives PS4 Exclusive DLC in `Control_DX11.exe` and `Control_DX12.exe`
 
-    - Unlocks the following:
+        - Unlocks the following:
 
-      - "Dr. Yoshimi Tokui's Guided Imagery Experience" side mission
+            - "Dr. Yoshimi Tokui's Guided Imagery Experience" side mission
 
-      - 2 exclusive outfits
+            - 2 exclusive outfits
 
-      - 2 rare mods
+            - 2 rare mods
 
-      - PC _pre-order_ bonus ✨
+            - PC _pre-order_ bonus ✨
 
-    - Searches [Epic Games]
+        - Searches [Epic Games]
 
-      - Search at offset(h) `00147AC0`
+            - Search at offset(h) `00147AC0`
 
-      - ```csharp
-        ...0xED, 0x75, 0x07...
-        ```
+            - ```csharp
+              ...0xED, 0x75, 0x07...
+              ```
 
-    - Patches
+        - Patch
 
-      - ```csharp
-        ...0xED, 0xEB, 0x07...
-        ```
+            - ```csharp
+              ...0xED, 0xEB, 0x07...
+              ```
 
-    - Searches [Steam / GOG]
+        - Searches [Steam / GOG]
 
-      - Search at offset(h) `00145B00` and `00145BA0`
+            - Search at offset(h) `00145B00` and `00145BA0`
 
-      - ```csharp
-        ...0xED, 0x75, 0x07...
-        ```
+            - ```csharp title="Part 1"
+              ...0xED, 0x75, 0x07...
+              ```
 
-      - ```csharp
-        ...0x00, 0x75, 0x2F, 0x48...
-        ```
+            - ```csharp title="Part 2"
+              ...0x00, 0x75, 0x2F, 0x48...
+              ```
 
-    - Patches
+        - Patch
 
-      - ```csharp
-        ...0xED, 0xEB, 0x07...
-        ```
+            - ```csharp title="Part 1"
+              ...0xED, 0xEB, 0x07...
+              ```
 
-      - ```csharp
-        ...0x00, 0x90, 0x90, 0x48...
-        ```
+            - ```csharp title="Part 2"
+              ...0x00, 0x90, 0x90, 0x48...
+              ```
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,114 +6,108 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Asoji Docs",
-  tagline: "documenting whatever i decide to throw my way",
-  url: "https://asoji.dev",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "Asoji", // Usually your GitHub org/user name.
-  projectName: "docs", // Usually your repo name.
+    title: "Asoji Docs",
+    tagline: "documenting whatever i decide to throw my way",
+    url: "https://asoji.dev",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "Asoji", // Usually your GitHub org/user name.
+    projectName: "docs", // Usually your repo name.
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css")
-        }
-      })
-    ]
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: "Asoji Docs",
-        // logo: {
-        //   alt: "My Site Logo",
-        //   src: "img/logo.svg"
-        // },
-        items: [
-          {
-            type: "doc",
-            docId: "controlpatcher/controlpatcher",
-            position: "left",
-            label: "Control Patcher"
-          }
-          // { to: "/blog", label: "Blog", position: "left" },
-          // {
-          //   href: "https://github.com/facebook/docusaurus",
-          //   label: "GitHub",
-          //   position: "right"
-          // }
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    // Please change this to your repo.
+                    editUrl:
+                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css")
+                }
+            })
         ]
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Control Patcher",
-                to: "/docs/controlpatcher"
-              }
-            ]
-          }
-          // {
-          //   title: "Community",
-          //   items: [
-          //     {
-          //       label: "Stack Overflow",
-          //       href: "https://stackoverflow.com/questions/tagged/docusaurus"
-          //     },
-          //     {
-          //       label: "Discord",
-          //       href: "https://discordapp.com/invite/docusaurus"
-          //     },
-          //     {
-          //       label: "Twitter",
-          //       href: "https://twitter.com/docusaurus"
-          //     }
-          //   ]
-          // },
-          // {
-          //   title: "More",
-          //   items: [
-          //     {
-          //       label: "Blog",
-          //       to: "/blog"
-          //     },
-          //     {
-          //       label: "GitHub",
-          //       href: "https://github.com/facebook/docusaurus"
-          //     }
-          //   ]
-          // }
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme
-      }
-    })
+    ],
+
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: "Asoji Docs",
+                // logo: {
+                //   alt: "My Site Logo",
+                //   src: "img/logo.svg"
+                // },
+                items: [
+                    {
+                        type: "doc",
+                        docId: "intro",
+                        position: "left",
+                        label: "Docs"
+                    }
+                    // { to: "/blog", label: "Blog", position: "left" },
+                    // {
+                    //   href: "https://github.com/facebook/docusaurus",
+                    //   label: "GitHub",
+                    //   position: "right"
+                    // }
+                ]
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Docs",
+                        items: [
+                            {
+                                label: "Control Patcher",
+                                to: "/docs/controlpatcher"
+                            }
+                        ]
+                    }
+                    // {
+                    //   title: "Community",
+                    //   items: [
+                    //     {
+                    //       label: "Stack Overflow",
+                    //       href: "https://stackoverflow.com/questions/tagged/docusaurus"
+                    //     },
+                    //     {
+                    //       label: "Discord",
+                    //       href: "https://discordapp.com/invite/docusaurus"
+                    //     },
+                    //     {
+                    //       label: "Twitter",
+                    //       href: "https://twitter.com/docusaurus"
+                    //     }
+                    //   ]
+                    // },
+                    // {
+                    //   title: "More",
+                    //   items: [
+                    //     {
+                    //       label: "Blog",
+                    //       to: "/blog"
+                    //     },
+                    //     {
+                    //       label: "GitHub",
+                    //       href: "https://github.com/facebook/docusaurus"
+                    //     }
+                    //   ]
+                    // }
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} asoji. Built with Docusaurus.`
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme
+            }
+        })
 };
 
 module.exports = config;
